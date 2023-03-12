d57a75181c355b3989028c7dd56de31a682e2f6e
Charles Swaleh <charlswall@live.com>
2023-03-12 14:44:55 +0300

    Completed inserting into the bears table

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES
+("Mr. Chocolate", 12, "M", "Brown", "Anxious", 1),
+("Rowdy", 4, "F", "White", "Calm", 0),
+("Tabitha", 10, "F", "White", "Sociable", 0),
+("Sergeant Brown", 15, "M", "Brown", "Peaceful", 1),
+("Melissa", 20, "F", "Black", "Sociable", 1),
+("Grinch", 21, "M", "White", "Unsociable", 0),
+("Wendy", 12, "F", "Black", "Talkative", 1),
+(NULL, NULL, "M", "Brown", "Anxious", 0);
